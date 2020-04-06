3907d1afb7824202ee1312891782fb66d9828ad2
Zachary Henderson <zachary.henderson114@gmail.com>
2020-04-06 16:01:05 -0500

    added header

---
+++ gitPage.js
@@ -0,0 +1,8 @@
+// const resume = document.getElementById('resume');
+// const linkedin = document.getElementById('linkedin');
+// const github = document.getElementById('github');
+// const youtube = document.getElementById('youtube');
+// const navbar = documsnt.getElementById('nav-bar');
+
+// resume.addEventListener('click', (e) => {});
+//
